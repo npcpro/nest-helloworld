0b48b94d3bef17b9de5cc78c2d8094c12be52b4c
npc <britandj7@gmail.com>
2020-01-10 16:56:48 +0700

    start

---
+++ src/assembler.ts
@@ -0,0 +1,10 @@
+import {AppService}    from './app.service';
+import {AppController} from './app.controller';
+
+const appService = new AppService();
+const appController = new AppController(appService);
+const hello = appController.getHello();
+
+console.log(appService,'appService');
+console.log(appController,'appController');
+console.log(hello);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { resolve } from 'path';
+
+async function bootstrap() {
+  const APP_DIRECTORY = resolve(__dirname,'..');
+  const app = await NestFactory.create(AppModule);
+        app.setViewEngine('hbs');
+        app.setBaseViewDir(resolve(APP_DIRECTORY,'views'));
+        app.useStaticAssets(resolve(APP_DIRECTORY,'public'));
+  await app.listen(3000);
+}
+bootstrap();
